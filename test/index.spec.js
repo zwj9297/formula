42ff1a39ef9c3b0c3870aea81b3db22530b65ecf
zwj9297 <790804121@qq.com>
2024-04-28 13:06:11 +0800

    refactor

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,63 +1,65 @@
-var expect = require('chai').expect
-var createFormula = require('../dist/formula').createFormula
+const { Formula, Math } = require('../dist/formula');
 
-const formula = createFormula()
+var expect = require('chai').expect;
 
 describe('简单运算', function () {
+  const formula = new Formula();
   it('相加：1+1=2?', function () {
-    return formula.calc('1+1').then((res) => {
-      expect(res).to.be.equal(2)
-    })
-  })
+    return formula.calculate('1+1').then((res) => {
+      expect(res).to.be.equal(2);
+    });
+  });
   it('小数相加：0.1+0.2=0.3?', function () {
-    return formula.calc('0.1+0.2').then((res) => {
-      expect(res).to.be.equal(0.3)
-    })
-  })
+    return formula.calculate('0.1+0.2').then((res) => {
+      expect(res).to.be.equal(0.3);
+    });
+  });
   it('相减：1-2=-1?', function () {
-    return formula.calc('1-2').then((res) => {
-      expect(res).to.be.equal(-1)
-    })
-  })
+    return formula.calculate('1-2').then((res) => {
+      expect(res).to.be.equal(-1);
+    });
+  });
   it('小数相减：0.1-0.2=0.1?', function () {
-    return formula.calc('0.1-0.2').then((res) => {
-      expect(res).to.be.equal(-0.1)
-    })
-  })
+    return formula.calculate('0.1-0.2').then((res) => {
+      expect(res).to.be.equal(-0.1);
+    });
+  });
   it('相乘：1*2=2?', function () {
-    return formula.calc('1*2').then((res) => {
-      expect(res).to.be.equal(2)
-    })
-  })
+    return formula.calculate('1*2').then((res) => {
+      expect(res).to.be.equal(2);
+    });
+  });
   it('小数相乘：0.1*0.2=0.02?', function () {
-    return formula.calc('0.1*0.2').then((res) => {
-      expect(res).to.be.equal(0.02)
-    })
-  })
+    return formula.calculate('0.1*0.2').then((res) => {
+      expect(res).to.be.equal(0.02);
+    });
+  });
   it('相除：1/2=0.5?', function () {
-    return formula.calc('1/2').then((res) => {
-      expect(res).to.be.equal(0.5)
-    })
-  })
+    return formula.calculate('1/2').then((res) => {
+      expect(res).to.be.equal(0.5);
+    });
+  });
   it('小数相除：0.1/0.2=0.5?', function () {
-    return formula.calc('0.1/0.2').then((res) => {
-      expect(res).to.be.equal(0.5)
-    })
-  })
-})
+    return formula.calculate('0.1/0.2').then((res) => {
+      expect(res).to.be.equal(0.5);
+    });
+  });
+});
 
 describe('复杂运算', function () {
+  const formula = new Formula();
   it('(1+(2-3)*4)/5=-0.6?', function () {
-    return formula.calc('(1+(2-3)*4)/5').then((res) => {
-      expect(res).to.be.equal(-0.6)
-    })
-  })
-})
+    return formula.calculate('(1+(2-3)*4)/5').then((res) => {
+      expect(res).to.be.equal(-0.6);
+    });
+  });
+});
 
 describe('数学函数', function () {
-  it('$Math.add(1,1)=2?', function () {
-    return formula.calc('$Math.add(1,1)').then((res) => {
-      expect(res).to.be.equal(2)
-    })
-  })
-})
+  const formula = new Formula({ plugins: [Math] });
+  it('$add(1,1)=2?', function () {
+    return formula.calculate('$add(1,1)').then((res) => {
+      expect(res).to.be.equal(2);
+    });
+  });
+});

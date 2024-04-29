a30333b9813167603a1d73518af4f9fdb5930f35
zwj9297 <790804121@qq.com>
2024-04-29 15:02:23 +0800

    refactor

--- test/index.spec.js
+++ test/index.spec.js
@@ -46,7 +46,7 @@ describe('简单运算', function () {
   });
 });
 
-describe('复杂运算', function () {
+describe('组合运算', function () {
   const formula = new Formula();
   it('(1+(2-3)*4)/5=-0.6?', function () {
     return formula.calculate('(1+(2-3)*4)/5').then((res) => {
@@ -57,9 +57,74 @@ describe('复杂运算', function () {
 
 describe('数学函数', function () {
   const formula = new Formula({ plugins: [Arithmetic] });
-  it('$add(1,1)=2?', function () {
+  it('求和：$add(1,1)=2?', function () {
     return formula.calculate('$add(1,1)').then((res) => {
       expect(res).to.be.equal(2);
+    });
+  });
+  it('求差：$minus(4,1)=3?', function () {
+    return formula.calculate('$minus(4,1)').then((res) => {
+      expect(res).to.be.equal(3);
+    });
+  });
+  it('求积：$multi(3,3)=9?', function () {
+    return formula.calculate('$multi(3,3)').then((res) => {
+      expect(res).to.be.equal(9);
+    });
+  });
+  it('求商：$div(8,5)=1.6?', function () {
+    return formula.calculate('$div(8,5)').then((res) => {
+      expect(res).to.be.equal(1.6);
+    });
+  });
+  it('求幂：$pow(2,8)=256?', function () {
+    return formula.calculate('$pow(2,8)').then((res) => {
+      expect(res).to.be.equal(256);
+    });
+  });
+  it('组合运算：$add($div(8,5), $multi(2,2))=5.6?', function () {
+    return formula.calculate('$add($div(8,5), $multi(2,2))').then((res) => {
+      expect(res).to.be.equal(5.6);
+    });
+  });
+  it('大于等于：$gte(1,2)=false?', function () {
+    return formula.calculate('$gte(1,2)').then((res) => {
+      expect(res).to.be.equal(false);
+    });
+  });
+  it('大于：$gt(2,1)=true?', function () {
+    return formula.calculate('$gt(2,1)').then((res) => {
+      expect(res).to.be.equal(true);
+    });
+  });
+  it('小于等于：$lte(3,3)=true?', function () {
+    return formula.calculate('$lte(3,3)').then((res) => {
+      expect(res).to.be.equal(true);
+    });
+  });
+  it('等于：$lt(3,3)=false?', function () {
+    return formula.calculate('$lt(3,3)').then((res) => {
+      expect(res).to.be.equal(false);
+    });
+  });
+  it('相等：$eq(1,1)=true?', function () {
+    return formula.calculate('$eq(1,1)').then((res) => {
+      expect(res).to.be.equal(true);
+    });
+  });
+  it('不相等：$neq(1,2)=true?', function () {
+    return formula.calculate('$neq(1,2)').then((res) => {
+      expect(res).to.be.equal(true);
+    });
+  });
+  it('转数字：$number("123")=123?', function () {
+    return formula.calculate('$number("123")').then((res) => {
+      expect(res).to.be.equal(123);
+    });
+  });
+  it('转数字：$number("1.23e2")=123?', function () {
+    return formula.calculate('$number("1.23e2")').then((res) => {
+      expect(res).to.be.equal(123);
     });
   });
 });
